229556a231a651ee90d8e4c0aca1879a79f8df34
Rafael Tosta <rafa.ecomp@gmail.com>
2026-09-13 14:23:18 -0300

    Update verification for Bazaar

--- checks/verify.sh
+++ checks/verify.sh
@@ -37,7 +37,7 @@ check_cmd gnome-shell "GNOME Shell instalado"
 check_cmd gnome-control-center "Configurações do GNOME instaladas"
 check_cmd nautilus "Nautilus instalado"
 check_cmd ptyxis "Ptyxis instalado"
-check_cmd gnome-software "GNOME Software instalado"
+check_cmd bazaar "Bazaar instalado"
 check_cmd code "Visual Studio Code instalado"
 check_cmd flatpak "Flatpak instalado"
 check_enabled gdm.service
